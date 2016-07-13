6ef254c0b8740c5dbf4ad9a7bef43e7b5b82d78b
Stuart Kam <stuart.kam17@gmail.com>
2016-07-12 17:38:13 -1000

    init

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resources :statuses
+
+  root 'statuses#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
